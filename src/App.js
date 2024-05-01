35569d24c76df6819a41e38bd572220a0f574edf
naman-398 <you@example.com>
2024-05-01 12:12:54 +0530

    social media icon changes

--- src/App.js
+++ src/App.js
@@ -11,24 +11,48 @@ import Ourgurantted from './components/Ourgurantted';
 import Whoarewe from './components/Whoarewe';
 import Slidersection from './components/Slidersection';
 import { Instagram, Linkedin2, Telegram, Twitter } from './icon/Icon';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Loader from './components/Loader';
 import Backtotop from './components/Backtotop';
+
 function App() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [showSocialMedia, setShowSocialMedia] = useState(true);
+
   useEffect(() => {
-    AOS.init(
-      {once: true,
-      duration: 1500,}
-    );
-  }, [])
+    AOS.init({
+      once: true,
+      duration: 1500,
+    });
+
+    let timeout;
+
+    const handleScroll = () => {
+      setScrollPosition(window.pageYOffset);
+      setShowSocialMedia(false);
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        setShowSocialMedia(true);
+      }, 500); 
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className=' bg-page-black overflow-hidden position-relative'>
-      <div className=' d-flex flex-column socialmedia bg-white position-fixed'>
-        <a href='https://twitter.com/login?lang=en' target='_blank'><Twitter/></a>
-        <a href='https://www.instagram.com/' target='_blank'><Instagram/></a>
-        <a href='https://telegram.org/' target='_blank'><Telegram/></a>
-        <a href='https://www.linkedin.com/feed/' target='_blank'><Linkedin2/></a>
-      </div>
+      {showSocialMedia && (
+        <div className=' d-flex flex-column socialmedia bg-white position-fixed'>
+          <a href='https://twitter.com/login?lang=en' target='_blank'><Twitter/></a>
+          <a href='https://www.instagram.com/' target='_blank'><Instagram/></a>
+          <a href='https://telegram.org/' target='_blank'><Telegram/></a>
+          <a href='https://www.linkedin.com/feed/' target='_blank'><Linkedin2/></a>
+        </div>
+      )}
       <Loader/>
       <Backtotop/>
       <Header/>
