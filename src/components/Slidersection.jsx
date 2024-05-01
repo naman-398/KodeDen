db8b58b8b3e3c7c9acb7bbe99cd860dd9f2e9699
naman-398 <you@example.com>
2024-05-01 10:15:54 +0530

    Swiper autoplay

--- src/components/Slidersection.jsx
+++ src/components/Slidersection.jsx
@@ -5,18 +5,20 @@ import sliderimg2 from "../assets/images/sliderimg2.webp";
 import sliderimg3 from "../assets/images/sliderimg3.webp";
 import { Linkedin, Nxtarrow, Prevarrow } from '../icon/Icon';
 import 'swiper/css';
-import 'swiper/css/navigation'; 
-import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/navigation';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import Commonbtn from '../commonbtn/Commonbtn';
+import { useRef, useState } from 'react';
 
 
 const Slidersection = () => {
+
     return (
         <div className='s5-padding'>
             <Container className=' position-relative'>
-                <h2 className='fs-3lg lh-72 fw-medium color-offwhite-e4 mb-5 text-center' data-aos="zoom-in">What our <span className='font-gradient'>students </span> are saying</h2>
+                <h2 className='fs-3lg lh-72 fw-medium color-offwhite-e4 mb-sm-5 mb-3 text-center' data-aos="zoom-in">What our <span className='font-gradient'>students </span> are saying</h2>
                 <Button className="swiper-button-prev d-sm-flex align-items-center justify-content-center d-none ">
-                    <Prevarrow/>
+                    <Prevarrow />
                 </Button>
                 <Swiper
                     spaceBetween={24}
@@ -26,25 +28,45 @@ const Slidersection = () => {
                         prevEl: '.swiper-button-prev',
                         nextEl: '.swiper-button-next',
                     }}
-                    modules={[Navigation]}
+                    modules={[Navigation,Autoplay]}
+                    autoplay
                     breakpoints={{
                         320: {
                             slidesPerView: 1,
+                            autoplay: {
+                                delay: 3000, 
+                                disableOnInteraction: false 
+                            }
                         },
                         500: {
                             slidesPerView: 1.5,
+                            autoplay: {
+                                delay: 3000, 
+                                disableOnInteraction: false 
+                            }
+                        },
+                        576: {
+                            slidesPerView: 1.5,
+                            autoplay: {
+                                delay: 3000, 
+                                disableOnInteraction: false 
+                            }
                         },
                         768: {
                             slidesPerView: 2,
+                            autoplay: false
                         },
                         992: {
-slidesPerView: 2.2,
+                            slidesPerView: 2.2,
+                            autoplay: false 
                         },
                         1200: {
                             slidesPerView: 2.7,
+                            autoplay: false 
                         },
                         1250: {
                             slidesPerView: 3,
+                            autoplay: false 
                         },
                     }}
                     onSlideChange={() => console.log('slide change')}
@@ -55,7 +77,7 @@ slidesPerView: 2.2,
                             <div className='slidercard'>
                                 <p className='fs-lg lh-36 text-white fw-normal mb-1'>Ross O’Rourke Founder, iC-1 Solution. CEO Agile Onboarding</p>
                                 <p className='fw-normal fs-2sm lh-25 color-offwhite3 mb-1'>Vitae elit dapibus facilisis magna. Sem amet sem ullamcorper augue magna egestas sit tempus, cras. Mauris gravida elit eget mi et vitae viverra duis eget. Aliquam, quisque gravida est pretium suscipit vel. Non blandit eget tortor netus pellentesque mauris, diam. </p>
-                                <p className='fw-semibold lh-25 fs-2sm color-offwhite'>Read More...</p>
+                                <p className='fw-semibold lh-25 fs-2sm color-offwhite cursor-pointer'>Read More...</p>
                                 <div className='d-flex align-items-center justify-content-between'>
                                     <div className='d-flex align-items-center gap-slider-name'>
                                         <img src={sliderimg1} alt="sliderimg1" className='sliderimg' />
@@ -64,7 +86,7 @@ slidesPerView: 2.2,
                                             <p className='fw-normal fs-1sm lh-20 color-offwhite3 mb-0'>@Ross O’Rourke</p>
                                         </div>
                                     </div>
-                                    <span className='me-4'>
+                                    <span className='me-4 cursor-pointer'>
                                         <Linkedin />
                                     </span>
                                 </div>
@@ -76,7 +98,7 @@ slidesPerView: 2.2,
                             <div className='slidercard'>
                                 <p className='fs-lg lh-36 text-white fw-normal mb-1'>“Luke O’Malley Tech Lead Agile Onboarding.”</p>
                                 <p className='fw-normal fs-2sm lh-25 color-offwhite3 mb-1'>I highly recommend kody Doherty. I have had the pleasure of working with him on variety of software projects over the past three years and his guidance has been intergral to my personal success. He is a driven, organized engineer who develops inspiring relationships with his students. </p>
-                                <p className='fw-semibold lh-25 fs-2sm color-offwhite'>Read More...</p>
+                                <p className='fw-semibold lh-25 fs-2sm color-offwhite cursor-pointer'>Read More...</p>
                                 <div className='d-flex align-items-center justify-content-between'>
                                     <div className='d-flex align-items-center gap-slider-name'>
                                         <img src={sliderimg2} alt="sliderimg1" className='sliderimg' />
@@ -85,7 +107,7 @@ slidesPerView: 2.2,
                                             <p className='fw-normal fs-1sm lh-20 color-offwhite3 mb-0'>@Luke O’Malley</p>
                                         </div>
                                     </div>
-                                    <span className='me-4'>
+                                    <span className='me-4 cursor-pointer'>
                                         <Linkedin />
                                     </span>
                                 </div>
@@ -97,7 +119,7 @@ slidesPerView: 2.2,
                             <div className='slidercard'>
                                 <p className='fs-lg lh-36 text-white fw-normal mb-1'>Leslie Sage Director of Data Science at DevResults</p>
                                 <p className='fw-normal fs-2sm lh-25 color-offwhite3 mb-1'>I had the pleasure of working with Kolton in 2019. In a team of a dozen people, Kolton stood out for his curiosity and drive. He executed ambitious projects with efficiency and attention to detail. He executed ambitious projects with efficiency and attention to detail.</p>
-                                <p className='fw-semibold lh-25 fs-2sm color-offwhite'>Read More...</p>
+                                <p className='fw-semibold lh-25 fs-2sm color-offwhite cursor-pointer'>Read More...</p>
                                 <div className='d-flex align-items-center justify-content-between'>
                                     <div className='d-flex align-items-center gap-slider-name'>
                                         <img src={sliderimg3} alt="sliderimg1" className='sliderimg' />
@@ -106,7 +128,7 @@ slidesPerView: 2.2,
                                             <p className='fw-normal fs-1sm lh-20 color-offwhite3 mb-0'>@Leslie Sage</p>
                                         </div>
                                     </div>
-                                    <span className='me-4'>
+                                    <span className='me-4 cursor-pointer'>
                                         <Linkedin />
                                     </span>
                                 </div>
@@ -118,7 +140,7 @@ slidesPerView: 2.2,
                             <div className='slidercard'>
                                 <p className='fs-lg lh-36 text-white fw-normal mb-1'>Ross O’Rourke Founder, iC-1 Solution. CEO Agile Onboarding</p>
                                 <p className='fw-normal fs-2sm lh-25 color-offwhite3 mb-1'>Vitae elit dapibus facilisis magna. Sem amet sem ullamcorper augue magna egestas sit tempus, cras. Mauris gravida elit eget mi et vitae viverra duis eget. Aliquam, quisque gravida est pretium suscipit vel. Non blandit eget tortor netus pellentesque mauris, diam. </p>
-                                <p className='fw-semibold lh-25 fs-2sm color-offwhite'>Read More...</p>
+                                <p className='fw-semibold lh-25 fs-2sm color-offwhite cursor-pointer'>Read More...</p>
                                 <div className='d-flex align-items-center justify-content-between'>
                                     <div className='d-flex align-items-center gap-slider-name'>
                                         <img src={sliderimg1} alt="sliderimg1" className='sliderimg' />
@@ -127,7 +149,7 @@ slidesPerView: 2.2,
                                             <p className='fw-normal fs-1sm lh-20 color-offwhite3 mb-0'>@Ross O’Rourke</p>
                                         </div>
                                     </div>
-                                    <span className='me-4'>
+                                    <span className='me-4 cursor-pointer'>
                                         <Linkedin />
                                     </span>
                                 </div>
@@ -139,7 +161,7 @@ slidesPerView: 2.2,
                             <div className='slidercard'>
                                 <p className='fs-lg lh-36 text-white fw-normal mb-1'>“Luke O’Malley Tech Lead Agile Onboarding.”</p>
                                 <p className='fw-normal fs-2sm lh-25 color-offwhite3 mb-1'>I highly recommend kody Doherty. I have had the pleasure of working with him on variety of software projects over the past three years and his guidance has been intergral to my personal success. He is a driven, organized engineer who develops inspiring relationships with his students. </p>
-                                <p className='fw-semibold lh-25 fs-2sm color-offwhite'>Read More...</p>
+                                <p className='fw-semibold lh-25 fs-2sm color-offwhite cursor-pointer'>Read More...</p>
                                 <div className='d-flex align-items-center justify-content-between'>
                                     <div className='d-flex align-items-center gap-slider-name'>
                                         <img src={sliderimg2} alt="sliderimg1" className='sliderimg' />
@@ -148,7 +170,7 @@ slidesPerView: 2.2,
                                             <p className='fw-normal fs-1sm lh-20 color-offwhite3 mb-0'>@Luke O’Malley</p>
                                         </div>
                                     </div>
-                                    <span className='me-4'>
+                                    <span className='me-4 cursor-pointer'>
                                         <Linkedin />
                                     </span>
                                 </div>
@@ -160,7 +182,7 @@ slidesPerView: 2.2,
                             <div className='slidercard'>
                                 <p className='fs-lg lh-36 text-white fw-normal mb-1'>Leslie Sage Director of Data Science at DevResults</p>
                                 <p className='fw-normal fs-2sm lh-25 color-offwhite3 mb-1'>I had the pleasure of working with Kolton in 2019. In a team of a dozen people, Kolton stood out for his curiosity and drive. He executed ambitious projects with efficiency and attention to detail. He executed ambitious projects with efficiency and attention to detail.</p>
-                                <p className='fw-semibold lh-25 fs-2sm color-offwhite'>Read More...</p>
+                                <p className='fw-semibold lh-25 fs-2sm color-offwhite cursor-pointer'>Read More...</p>
                                 <div className='d-flex align-items-center justify-content-between'>
                                     <div className='d-flex align-items-center gap-slider-name'>
                                         <img src={sliderimg3} alt="sliderimg1" className='sliderimg' />
@@ -169,7 +191,7 @@ slidesPerView: 2.2,
                                             <p className='fw-normal fs-1sm lh-20 color-offwhite3 mb-0'>@Leslie Sage</p>
                                         </div>
                                     </div>
-                                    <span className='me-4'>
+                                    <span className='me-4 cursor-pointer'>
                                         <Linkedin />
                                     </span>
                                 </div>
@@ -179,10 +201,10 @@ slidesPerView: 2.2,
 
                 </Swiper>
                 <Button className="swiper-button-next d-sm-flex align-items-center justify-content-center d-none ">
-                    <Nxtarrow/>
+                    <Nxtarrow />
                 </Button>
-                <div className=' d-flex justify-content-center mt-5 pt-1' data-aos="zoom-in">
-                    <Commonbtn/>
+                <div className=' d-flex justify-content-center mt-sm-5 mt-3 pt-sm-1' data-aos="zoom-in">
+                    <Commonbtn />
                 </div>
             </Container>
         </div>
